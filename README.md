0e20aaba880eaa51146f7bcd3af9217be7561d26
AbelMaireg <abelrighthere@gmail.com>
2025-12-07 09:55:28 +0300

    perf: day-7 6x faster

--- README.md
+++ README.md
@@ -17,9 +17,9 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | [Day 4](./src/bin/04.rs) | `303.3µs` | `4.9ms` |
 | [Day 5](./src/bin/05.rs) | `97.1µs` | `20.6µs` |
 | [Day 6](./src/bin/06.rs) | `103.5µs` | `94.8µs` |
-| [Day 7](./src/bin/07.rs) | `126.5µs` | `126.3µs` |
+| [Day 7](./src/bin/07.rs) | `22.0µs` | `22.3µs` |
 
-**Total: 157.96ms**
+**Total: 157.75ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,78 +1,40 @@
-use std::collections::HashMap;
-
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let start = input.lines().next()?.find('S')?;
-    let spliters: Vec<Vec<usize>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .enumerate()
-                .filter_map(|(i, c)| if c == '^' { Some(i) } else { None })
-                .collect()
-        })
-        .filter(|v: &Vec<usize>| !v.is_empty())
-        .collect();
+    let mut splits = vec![true; input.lines().next()?.chars().count()];
+    splits[input.lines().next()?.find('S')?] = true;
 
-    let mut untouched = 0;
-
-    let _ = spliters
-        .iter()
-        .fold(HashMap::from([(start, 1u64)]), |mut acc, spliters| {
-            for s in spliters {
-                if let Some(&count) = acc.get(s)
-                    && count == 0
-                {
-                    untouched += 1;
-                    continue;
-                }
-                let count = *acc.get(s).unwrap_or(&0);
-                *acc.entry(s + 1).or_default() += count;
-                *acc.entry(s - 1).or_default() += count;
-                *acc.get_mut(s).unwrap() = 0;
+    let mut count = 0;
+    input.lines().for_each(|line| {
+        line.chars().enumerate().for_each(|(s, c)| {
+            if c == '^' && splits[s] {
+                splits[s + 1] = true;
+                splits[s - 1] = true;
+                splits[s] = false;
+                count += 1;
             }
-            acc
-        });
+        })
+    });
 
-    Some(spliters.iter().flatten().count() as u64 - untouched)
+    count.into()
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let start = input.lines().next()?.find('S')?;
-    let spliters: Vec<Vec<usize>> = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .enumerate()
-                .filter_map(|(i, c)| if c == '^' { Some(i) } else { None })
-                .collect()
-        })
-        .filter(|v: &Vec<usize>| !v.is_empty())
-        .collect();
+    let mut splits = vec![0; input.lines().next()?.chars().count()];
+    splits[input.lines().next()?.find('S')?] = 1;
 
-    let mut untouched = 0;
-
-    spliters
-        .iter()
-        .fold(HashMap::from([(start, 1u64)]), |mut acc, spliters| {
-            for s in spliters {
-                if let Some(&count) = acc.get(s)
-                    && count == 0
-                {
-                    untouched += 1;
-                    continue;
-                }
-                let count = *acc.get(s).unwrap_or(&0);
-                *acc.entry(s + 1).or_default() += count;
-                *acc.entry(s - 1).or_default() += count;
-                *acc.get_mut(s).unwrap() = 0;
+    input.lines().for_each(|line| {
+        line.chars().enumerate().for_each(|(s, c)| {
+            if c == '^' && splits[s] != 0 {
+                let beams = splits[s];
+                splits[s + 1] += beams;
+                splits[s - 1] += beams;
+                splits[s] = 0;
             }
-            acc
         })
-        .into_values()
-        .sum::<u64>()
-        .into()
+    });
+
+    splits.iter().sum::<u64>().into()
 }
 
 #[cfg(test)]
@@ -88,6 +50,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(40));
     }
 }
